1730682b344ad03922aa57f2f93bb414c79a0986
Sebastian Weiß <s.weiss@bitrebels.de>
2024-04-13 12:44:18 +0200

    add folders and first player script

---
+++ LudumDare55/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+
+    // Frame-rate independent update cycle
+    private void FixedUpdate()
+    {
+        
+    }
+}
