c2c5316040a1d62039bb2f760b7819b1fe5dc653
BenniBasmaci <benjaminbasmaci93@gmail.com>
2024-04-13 20:29:04 +0200

    Implement summon ground

--- LudumDare55/Assets/Scripts/ChargeSummon.cs
+++ LudumDare55/Assets/Scripts/ChargeSummon.cs
@@ -4,11 +4,19 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Tilemaps;
 
 public class ChargeSummon : MonoBehaviour, IPointerDownHandler
 {
-    public int MaxCharges = 1;
-    public int CurrentCharges = 1;
+    public Texture2D customCursorTexture;
+    public int maxCharges = 1;
+    public int currentCharges = 1;
+
+    private bool changeText = false;
+
+    private bool customCursorActive = false;
+    public TileBase tileToPlace;
+    public Tilemap tileMap;
 
     // Start is called before the first frame update
     void Start()
@@ -19,13 +27,53 @@ public class ChargeSummon : MonoBehaviour, IPointerDownHandler
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonUp(0))
+        if (customCursorActive)
         {
+            // Check for right click to switch back to normal cursor
+            if (Input.GetMouseButtonDown(1))
+            {
+                Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+                customCursorActive = false;
+                Debug.Log("DEACTIVATE");
+                return;
+            }
+
+            // Check for left click
+            if (Input.GetMouseButtonDown(0))
+            {
+                Debug.Log("CLICK DETECTED");
+                Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                mousePosition.z = 0;
+
+                var cellVector3 = tileMap.WorldToCell(mousePosition);
+                Debug.Log("CELLVECTOR: " + cellVector3.ToString());
+
+                var clickedTile = tileMap.GetTile(cellVector3);
+                
+
+
+                if (clickedTile == null)
+                {
+                    tileMap.SetTile(cellVector3, tileToPlace);
+
+                    // Example: Change the leftNum value
+                    currentCharges--;
+                    Debug.Log("Charges changed: " + currentCharges + " gameObject = ");
+                }
+                else
+                {
+                    Debug.Log("CLICKED TILE: " + clickedTile.ToString());
+                }
+            }
         }
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        Debug.Log("MOUSEDOWN!");
+        Cursor.SetCursor(customCursorTexture, Vector2.zero, CursorMode.Auto);
+        customCursorActive = true;
+        Debug.Log("ACTIVATE");
     }
+
+
 }
