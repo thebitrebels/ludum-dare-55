62f2e2d6234dcfa4259d0f3dfc14739b2ae1bbdb
Sebastian Weiß <s.weiss@bitrebels.de>
2024-04-14 22:10:27 +0200

    level 3 elephant draining

--- LudumDare55/Assets/Elephant.cs
+++ LudumDare55/Assets/Elephant.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Elephant : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public LayerMask waterLayer;
+    public float drainSpeed = 0.01f;
+    private List<Tilemap> _waterTilemaps;
+
+    private Tilemap _drainingMap;
+    private float _drained;
+    
+    private void Start()
     {
-        
+        _waterTilemaps = GameObject.FindGameObjectsWithTag("Water").Select(x => x.GetComponent<Tilemap>()).ToList();
+        var closestWater = _waterTilemaps.OrderBy((map) => Vector3.Distance(map.transform.position, transform.position)).FirstOrDefault();
+        if (Vector3.Distance(closestWater.transform.position, transform.position) > 15f)
+        {
+            return;
+        }
+        Drain(closestWater);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Drain(Tilemap closestWater)
     {
-        
+        _drained = 0f;
+        _drainingMap = closestWater;
+    }
+
+    private void FixedUpdate()
+    {
+        if (_drainingMap != null)
+        {
+            _drainingMap.transform.Translate(drainSpeed * Vector3.down);
+            _drained += drainSpeed;
+            if (_drained >= 1f)
+            {
+                _drainingMap = null;
+                Destroy(gameObject);
+            }
+        }
     }
 }
