edd01b4285bad589034f05bd79e87966588d583d
Sebastian Weiß <s.weiss@bitrebels.de>
2024-04-14 16:21:21 +0200

    add goal to level 1 and update player prefab

--- LudumDare55/Assets/Scripts/Goal.cs
+++ LudumDare55/Assets/Scripts/Goal.cs
@@ -1,25 +1,12 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class Goal : MonoBehaviour
 {
     public string nextSceneName;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        SceneManager.LoadScene(nextSceneName);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(nextSceneName);
     }
 
 }
