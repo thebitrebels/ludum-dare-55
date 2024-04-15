6b33dd73e0e98887214c6d524403b70201d06205
Sebastian Weiß <s.weiss@bitrebels.de>
2024-04-15 13:43:01 +0200

    fix unused script

--- LudumDare55/Assets/Scripts/LateralMoveAlterSpeed.cs
+++ LudumDare55/Assets/Scripts/LateralMoveAlterSpeed.cs
@@ -8,14 +8,8 @@ public class LateralMoveAlterSpeed : LateralMovementBase
     public int rightBorderForHighspeed = 0;
     public int highspeedFactor = 5;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
-    void Update()
+    private new void Update()
     {
         RecalculateLeftRightFactor();
 
