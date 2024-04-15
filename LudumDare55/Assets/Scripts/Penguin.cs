ea59f08b785af80a742e63d1ee4956cdf9785b94
Lucas Engelke <l.engelke@bitrebels.de>
2024-04-15 12:21:32 +0200

    add basic penguin summon

---
+++ LudumDare55/Assets/Scripts/Penguin.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Tilemaps;
+
+public class Penguin : MonoBehaviour
+{
+    public LayerMask waterLayer;
+    public float drainSpeed = 0.01f;
+    private List<Tilemap> _waterTilemaps;
+
+    private Tilemap _drainingMap;
+    private float _drained;
+
+    private void Start()
+    {
+        _waterTilemaps = GameObject.FindGameObjectsWithTag("Water").Select(x => x.GetComponent<Tilemap>()).ToList();
+        var closestWater = _waterTilemaps.OrderBy((map) => Vector3.Distance(map.transform.position, transform.position)).FirstOrDefault();
+        if (Vector3.Distance(closestWater.transform.position, transform.position) > 15f)
+        {
+            return;
+        }
+        Drain(closestWater);
+    }
+
+    private void Drain(Tilemap closestWater)
+    {
+        _drained = 0f;
+        _drainingMap = closestWater;
+    }
+
+    private void FixedUpdate()
+    {
+        if (_drainingMap != null)
+        {
+            _drainingMap.transform.Translate(drainSpeed * Vector3.down);
+            _drained += drainSpeed;
+            if (_drained >= 1f)
+            {
+                _drainingMap.GetComponent<BoxCollider2D>().enabled = false;
+                _drainingMap = null;
+
+            }
+        }
+    }
+}
---
+++ LudumDare55/Assets/Scripts/Summonings/PenguinSummon.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class PenguinSummon : SummoningBase
+{
+    public int costRed = 1;
+    public int costYellow = 1;
+    public int costBlue = 1;
+    public TileBase tileToPlace;
+
+    public override bool CanPerformSummon()
+    {
+        return ResourceManager.instance.CanPerformSummoning(costRed, costYellow, costBlue);
+    }
+
+    public override bool CanPerformSummonAt(Vector3Int worldToCellVector, Tilemap tilemap)
+    {
+        if (!CanPerformSummon())
+            return false;
+
+        var clickedTile = tilemap.GetTile(worldToCellVector);
+        return clickedTile == null;
+    }
+
+    public override void PerformSummon(Vector3Int worldToCellVector, Tilemap tilemap)
+    {
+        // Drains Water
+        tilemap.SetTile(worldToCellVector, tileToPlace);
+        ResourceManager.instance.PerformSummoning(costRed, costYellow, costBlue);
+        StartCoroutine(ClearTileWithDelay(tilemap, worldToCellVector));
+    }
+
+    static IEnumerator ClearTileWithDelay(Tilemap tilemap, Vector3Int pos)
+    {
+        yield return new WaitForSeconds(3);
+        tilemap.SetTile(pos, null);
+    }
+}
